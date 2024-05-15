061ced07d1042cbba3527037c157d03037246a6e
Dedaldino Papelo <dedaldino.papelo@okayulatech.ao>
2024-05-15 22:35:11 +0100

    enhance the test in the MovieViewModelTest

--- app/src/test/java/com/example/movieinfoapp/fake/MovieViewModelTest.kt
+++ app/src/test/java/com/example/movieinfoapp/fake/MovieViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.example.movieinfoapp.fake
 
 import com.example.movieinfoapp.rules.TestDispatcherRule
+import com.example.movieinfoapp.ui.screens.MovieUiState
 import com.example.movieinfoapp.ui.screens.MovieViewModel
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert
@@ -14,6 +15,9 @@ class MovieViewModelTest {
     fun movieViewModel_getMovies_verifyMovieUiStateSuccess() =
         runTest {
             val movieViewModel = MovieViewModel(moviesRepository = FakeNetworkMoviesRepository())
-            Assert.assertEquals(FakeDataSource.movieList.results, movieViewModel.movieUiState)
+            Assert.assertEquals(
+                MovieUiState.Success(FakeDataSource.movieList.results),
+                movieViewModel.movieUiState
+            )
         }
 }
